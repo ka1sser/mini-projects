b5c35f3246ccd2bf84e974c7e2fcc7c31b7b3e23
ka1sser <serenokaisser.dev@gmail.com>
2025-02-20 10:58:10 +0800

    Learning: Polymorphism

---
+++ oop/shape.py
@@ -0,0 +1,50 @@
+# Topic: Polymorphism
+
+from abc import ABC, abstractmethod
+
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+
+class Square(Shape):
+
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side**2
+
+
+class Triangle(Shape):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius)
+        self.topping = topping
+
+
+shapes = [Circle(5), Square(3), Triangle(6, 7), Pizza("pepperoni", 15)]
+
+for shape in shapes:
+    print(f"{shape.area()} cm^2")
