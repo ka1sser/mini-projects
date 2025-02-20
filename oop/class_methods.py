a39ad0d857c4504d2c9873365e7da2d6b2057bea
ka1sser <serenokaisser.dev@gmail.com>
2025-02-20 11:20:27 +0800

    Learning: OOP | Class Methods

---
+++ oop/class_methods.py
@@ -0,0 +1,38 @@
+# Topic: OOP | Class Methods
+
+
+class Student:
+
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
+
+    # INSTANCE METHOD
+    def get_info(self):
+        return f"{self.name} has GPA: {self.gpa}"
+
+    @classmethod
+    def get_count(cls):
+        return f"Total number of students: {cls.count}"
+
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            average_gpa = cls.total_gpa / cls.count
+
+            return f"Average GPA: {average_gpa:.2f}"
+
+
+student1 = Student("Spongebob", 3.2)
+student2 = Student("Patrick", 2.0)
+student3 = Student("Sandy", 4.0)
+
+print(Student.get_count())
+print(Student.get_average_gpa())
